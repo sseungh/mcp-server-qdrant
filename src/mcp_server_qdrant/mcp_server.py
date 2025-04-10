8a7bd91211dd25276b00c211b0e8bd6e15ab9a21
Kacper Łukawski <lukawski.kacper@gmail.com>
2025-04-10 18:20:23 +0200

    Subclass the `QdrantMCPServer` to support single/multi-collection setups

--- src/mcp_server_qdrant/mcp_server.py
+++ src/mcp_server_qdrant/mcp_server.py
@@ -1,3 +1,4 @@
+import abc
 import json
 import logging
 from typing import Any, List
@@ -17,9 +18,10 @@ logger = logging.getLogger(__name__)
 
 # FastMCP is an alternative interface for declaring the capabilities
 # of the server. Its API is based on FastAPI.
-class QdrantMCPServer(FastMCP):
+class QdrantMCPServer(FastMCP, abc.ABC):
     """
-    A MCP server for Qdrant.
+    An MCP server for Qdrant using a single collection specified in
+    the configuration.
     """
 
     def __init__(
@@ -48,12 +50,85 @@ class QdrantMCPServer(FastMCP):
 
         self.setup_tools()
 
+    @abc.abstractmethod
+    async def find(self, *args, **kwargs) -> Any:
+        """
+        An abstract method for finding memories in Qdrant.
+        Each implementation should define its own set of parameters,
+        so they are visible in the MCP client.
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    async def store(self, *args, **kwargs) -> Any:
+        """
+        An abstract method for storing memories in Qdrant.
+        Each implementation should define its own set of parameters,
+        so they are visible in the MCP client.
+        """
+        raise NotImplementedError
+
     def format_entry(self, entry: Entry) -> str:
         """
         Feel free to override this method in your subclass to customize the format of the entry.
         """
         entry_metadata = json.dumps(entry.metadata) if entry.metadata else ""
         return f"<entry><content>{entry.content}</content><metadata>{entry_metadata}</metadata></entry>"
+
+    def setup_tools(self):
+        """
+        Register the tools in the server.
+        """
+        self.add_tool(
+            self.find,
+            name="qdrant-find",
+            description=self.tool_settings.tool_find_description,
+        )
+
+        if not self.qdrant_settings.read_only:
+            # Those methods can modify the database
+            self.add_tool(
+                self.store,
+                name="qdrant-store",
+                description=self.tool_settings.tool_store_description,
+            )
+
+
+class MultiCollectionQdrantMCPServer(QdrantMCPServer):
+    """
+    An MCP server for Qdrant accepting collection name as a tool parameter,
+    so multiple collections can be used for different kinds of memories.
+    """
+
+    async def find(
+        self,
+        ctx: Context,
+        query: str,
+        collection_name: str,
+    ) -> List[str]:
+        """
+        Find memories in Qdrant.
+        :param ctx: The context for the request.
+        :param query: The query to use for the search.
+        :param collection_name: The name of the collection to search in, optional. If not provided,
+                                the default collection is used.
+        :return: A list of entries found.
+        """
+        await ctx.debug(f"Finding results for query '{query}' in {collection_name}")
+
+        entries = await self.qdrant_connector.search(
+            query,
+            collection_name=collection_name,
+            limit=self.qdrant_settings.search_limit,
+        )
+        if not entries:
+            return [f"No information found for the query '{query}'"]
+        content = [
+            f"Results for the query '{query}'",
+        ]
+        for entry in entries:
+            content.append(self.format_entry(entry))
+        return content
 
     async def store(
         self,
@@ -77,44 +152,32 @@ class QdrantMCPServer(FastMCP):
         await ctx.debug(f"Storing information {information} in Qdrant")
 
         entry = Entry(content=information, metadata=metadata)
-
         await self.qdrant_connector.store(entry, collection_name=collection_name)
-        if collection_name:
-            return f"Remembered: {information} in collection {collection_name}"
-        return f"Remembered: {information}"
+        return f"Remembered: {information} in collection {collection_name}"
 
-    async def store_with_default_collection(
-        self,
-        ctx: Context,
-        information: str,
-        metadata: Metadata = None,  # type: ignore
-    ) -> str:
-        assert self.qdrant_settings.collection_name is not None
-        return await self.store(
-            ctx, information, self.qdrant_settings.collection_name, metadata
-        )
+
+class DefaultCollectionQdrantMCPServer(QdrantMCPServer):
+    """
+    An MCP server for Qdrant using a single collection specified in
+    the configuration.
+    """
 
     async def find(
         self,
         ctx: Context,
         query: str,
-        collection_name: str,
     ) -> List[str]:
         """
-        Find memories in Qdrant.
+        Find memories in Qdrant using the default collection.
         :param ctx: The context for the request.
         :param query: The query to use for the search.
-        :param collection_name: The name of the collection to search in, optional. If not provided,
-                                the default collection is used.
         :return: A list of entries found.
         """
         await ctx.debug(f"Finding results for query {query}")
-        if collection_name:
-            await ctx.debug(f"Overriding the collection name with {collection_name}")
 
         entries = await self.qdrant_connector.search(
             query,
-            collection_name=collection_name,
+            collection_name=self.qdrant_settings.collection_name,
             limit=self.qdrant_settings.search_limit,
         )
         if not entries:
@@ -126,46 +189,45 @@ class QdrantMCPServer(FastMCP):
             content.append(self.format_entry(entry))
         return content
 
-    async def find_with_default_collection(
+    async def store(
         self,
         ctx: Context,
-        query: str,
-    ) -> List[str]:
-        assert self.qdrant_settings.collection_name is not None
-        return await self.find(ctx, query, self.qdrant_settings.collection_name)
-
-    def setup_tools(self):
+        information: str,
+        metadata: Metadata = None,  # type: ignore
+    ) -> str:
         """
-        Register the tools in the server.
+        Store some information in Qdrant in a default collection.
+        :param ctx: The context for the request.
+        :param information: The information to store.
+        :param metadata: JSON metadata to store with the information, optional.
+        :return: A message indicating that the information was stored.
         """
+        await ctx.debug(f"Storing information {information} in Qdrant")
 
-        # Register the tools depending on the configuration
+        entry = Entry(content=information, metadata=metadata)
+        await self.qdrant_connector.store(
+            entry, collection_name=self.qdrant_settings.collection_name
+        )
+        return f"Remembered: {information}"
 
-        if self.qdrant_settings.collection_name:
-            self.add_tool(
-                self.find_with_default_collection,
-                name="qdrant-find",
-                description=self.tool_settings.tool_find_description,
-            )
-        else:
-            self.add_tool(
-                self.find,
-                name="qdrant-find",
-                description=self.tool_settings.tool_find_description,
-            )
 
-        if not self.qdrant_settings.read_only:
-            # Those methods can modify the database
-
-            if self.qdrant_settings.collection_name:
-                self.add_tool(
-                    self.store_with_default_collection,
-                    name="qdrant-store",
-                    description=self.tool_settings.tool_store_description,
-                )
-            else:
-                self.add_tool(
-                    self.store,
-                    name="qdrant-store",
-                    description=self.tool_settings.tool_store_description,
-                )
+def create_mcp_server(
+    tool_settings: ToolSettings,
+    qdrant_settings: QdrantSettings,
+    embedding_provider_settings: EmbeddingProviderSettings,
+) -> QdrantMCPServer:
+    """
+    Create an instance of the appropriate MCP server based on the configuration.
+    """
+    if qdrant_settings.collection_name:
+        return DefaultCollectionQdrantMCPServer(
+            tool_settings,
+            qdrant_settings,
+            embedding_provider_settings,
+        )
+    else:
+        return MultiCollectionQdrantMCPServer(
+            tool_settings,
+            qdrant_settings,
+            embedding_provider_settings,
+        )
