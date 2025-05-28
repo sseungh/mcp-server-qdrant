b5ce8c5d0cb6f6dde7f84c93f757f5043cb6505a
Andrey Vasnetsov <andrey@vasnetsov.com>
2025-05-28 18:06:50 +0200

    rollback debug code

--- src/mcp_server_qdrant/server.py
+++ src/mcp_server_qdrant/server.py
@@ -1,24 +1,12 @@
 from mcp_server_qdrant.mcp_server import QdrantMCPServer
 from mcp_server_qdrant.settings import (
     EmbeddingProviderSettings,
-    FilterableField,
     QdrantSettings,
     ToolSettings,
 )
 
-qdrant_settings = QdrantSettings(
-    filterable_fields=[
-        FilterableField(
-            name="color",
-            field_type="keyword",
-            condition="==",
-            description="The color of the object",
-        ),
-    ],
-)
-
 mcp = QdrantMCPServer(
     tool_settings=ToolSettings(),
-    qdrant_settings=qdrant_settings,
+    qdrant_settings=QdrantSettings(),
     embedding_provider_settings=EmbeddingProviderSettings(),
 )
